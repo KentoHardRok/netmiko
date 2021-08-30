091b8f96d95402757670a6baf993b13364d87295
Tom Wilent <tom.wilent@gmail.com>
2021-08-30 13:56:29 +0000

    added junos backup config

--- juniper_backup.py
+++ juniper_backup.py
@@ -1,51 +1,47 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-
 from netmiko import ConnectHandler
 from colorama import Fore, Style
 import os.path
 
-num_of_dev = 14
+num_of_dev = 5
 dev_proname = "R"
 dev_list = {}
-hostnet = "192.168.100.100"
+hostnet = "192.168.100.110"
+
 
 def ipinc(ip, n):
-    ip_list=[]
+    ip_list = []
     octets = ip.split(".")
     int_octets = list(map(int, octets))
     for i in range(n):
         int_octets[3] += 1
         ip_list.append('.'.join(list((map(str, int_octets)))))
-    return(ip_list)
+    return (ip_list)
+
 
 net = ipinc(hostnet, num_of_dev)
 
 for i in range(num_of_dev):
     dev_list[f'{dev_proname}{i+1}'] = {
-        'device_type':'cisco_ios',
+        'device_type': 'juniper',
         'ip': net[i],
         'username': 'tomw',
         'password': 'mR7h0M@$',
     }
 
-save_path = './backups' #Path to files
+save_path = './backups'  #Path to files
 if not os.path.exists('./backups'):
     os.makedirs('./backups')
 for router in dev_list.values():
     print(Fore.YELLOW + "#" * 70 + Style.RESET_ALL)
-    try:
-        with ConnectHandler(**router) as device:
-            # Defining the Path of a File and Creating the Filename for a File
-            completeName = os.path.join(save_path, device.host + '_run_config.txt')
-            # Assigning the Result of show run to output1
-            output1 = device.send_command('show run')
-            file1 = open(completeName, 'w')
-            file1.write(output1)
-            file1.close()
-            print(device.host, "has completed")
-    except Exception:
-        pass
-
-
+    with ConnectHandler(**router) as device:
+        # Defining the Path of a File and Creating the Filename for a File
+        completeName = os.path.join(save_path, device.host + '_run_config.txt')
+        # Assigning the Result of show run to output1
+        output1 = device.send_command('show config')
+        file1 = open(completeName, 'w')
+        file1.write(output1)
+        file1.close()
+        print(device.host, "has completed")
